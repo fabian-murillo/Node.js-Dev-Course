5227fa982d9c811e76b890ddd5cc5261052a563d
Fabian <murillo.a.fabian@gmail.com>
2018-06-02 19:52:08 -0400

    adding lodash

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,15 +2,22 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 // Using a relative path to tell node where to look for this file:
 const notes = require('./notes.js')
 
-var res = notes.addNote();
-console.log(res);
+// console.log(_.isString(true));
+// console.log(_.isString('Fabian'));
+var filteredArray = _.uniq(['Fabian, 1, Fabian, 1, 2, 3, 4']);
+console.log(filteredArray);
+
+
+// var res = notes.addNote();
+// console.log(res);
 
 // Calling the function we created in notes.js for our 1st solo challenge:
-var sum = notes.add(3,2);
-console.log(sum);
+// var sum = notes.add(3,2);
+// console.log(sum);
 
 // var user = os.userInfo();
 
