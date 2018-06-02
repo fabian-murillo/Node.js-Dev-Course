eac2ec3bab8bb145047868b4888986e0efe0c3e2
Fabian <murillo.a.fabian@gmail.com>
2018-06-02 19:27:29 -0400

    1st solo challenge

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,12 +5,18 @@ const os = require('os');
 // Using a relative path to tell node where to look for this file:
 const notes = require('./notes.js')
 
-var user = os.userInfo();
+var res = notes.addNote();
+console.log(res);
 
-// Appending the data we've made available here through the require function and the note.js file:
-fs.appendFile('greeting.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
-    if (err) {
-        console.log('Unable to write to file');
-    }
-});
+var sum = notes.add(3,2);
+console.log(sum);
+
+// var user = os.userInfo();
+
+// // Appending the data we've made available here through the require function and the note.js file:
+// fs.appendFile('greeting.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
+//     if (err) {
+//         console.log('Unable to write to file');
+//     }
+// });
 
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,8 +1,11 @@
 console.log('Starting notes.js');
 
-// Use module.exports to make data available to app.js:
-module.exports.age = 28;
-// exports, is actually an object, which is really a property, on the larger module object:
-console.log(module)
-// so module is an object, with a propery on it called exports, that correlates to another 
-// object, which we can then append data to. That object, is what module.exports points to...
+// Remember arrow functions do not bind the 'this' keyword, or the arguments array
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New note';
+}
+
+module.exports.add = (a,b) => {
+    return a + b;
+}
