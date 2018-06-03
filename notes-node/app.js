c0554a4431bfcbbf92294621dba5e1ad4ca3b7ff
Fabian <murillo.a.fabian@gmail.com>
2018-06-03 12:28:46 -0400

    Passing command line data to our app using process.argv

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,11 +1,23 @@
-/* 
-I was mistaken...in an earlier commit, I commented that I had deleted
-my node modules folder, before pushing to Github; this statement was
-incorrect, I wasn't aware...still learning to use VS code, Github integration 
-is cool....but stuff like that happens too...which sucks :(
-*/
 const fs = require('fs');
 const _ = require('lodash')
 
 const notes = require('./notes.js')
 
+// Accessing CL arg's app was intitialized with using process.argv: 
+console.log(process.argv); // short for arguments vector, similar to an args array
+// Grabbing the 3rd item in the arguments vector:
+var command = process.argv[2];
+// Logging it to the console:
+console.log('Command: ', command);
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading notes');
+} else if (command === 'remove') {
+    console.log('Removing note');
+}else {
+    console.log('Command not recognized');
+}
