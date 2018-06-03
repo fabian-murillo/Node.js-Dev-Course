7382a120c127be0e85a4d9683680047e92c07404
Fabian <murillo.a.fabian@gmail.com>
2018-06-02 20:37:39 -0400

    'adding comment about VS code integration...still learning...but I like it'

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,5 +1,9 @@
-console.log('Starting app.js');
-
+/* 
+I was mistaken...in an earlier commit, I commented that I had deleted
+my node modules folder, before pushing to Github; this statement was
+incorrect, I wasn't aware...still learning to use VS code, Github integration 
+is cool....but stuff like that happens too...which sucks :(
+*/
 const fs = require('fs');
 const _ = require('lodash')
 
