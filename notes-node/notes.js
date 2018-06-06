8d4f2acdf3ee881ddbdb71e6db83064091080fae
Fabian <murillo.a.fabian@gmail.com>
2018-06-06 12:42:29 -0400

    expanding the addNote function

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -6,13 +6,29 @@ var addNote = (title, body) => {
     var notes = [];
     // represents a new note:
     var note = {
-        title, // ES6, both items are the same, no colon needed
+        title, // ES6, both names are the same, no colon needed
         body
+    };
+    // Try/Catch statement, incase file doesn't exist yet!
+    // Try adding a note to the file:
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+    // If the file doesn't exist that's ok, cuz we have an empty array anyways
+    // So we parse the returned data into the notes array
+        notes = JSON.parse(notesString);
+    } catch (error) {
+
     }
 
-    notes.push(note);
+    // Eventually will store an array w/ all the notes that already exist
+    // inside the notes array, w/ the same title
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
     // Add a new note to the file:
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));    
+    }   
 };
 
 var getAll = () => {
